6e6b43ef6c4d187095482f223d602790c74f562c
danielabilbao <81271853+danielabilbao@users.noreply.github.com>
2021-09-10 20:10:02 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import Historia from "./components/Historia/Historia";
+//import Opciones from "./components/Opciones/Opciones";
+//import Recordatorio from "./components/Recordatorio/Recordatorio";
+import Main from "./components/main";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/Historia/Historia.js
@@ -0,0 +1,7 @@
+import React, {Component}from "React";
+
+export default class Historia extends Component{
+render(){
+    return <> </>
+}
+}
